33121851b8eeb7439e4c288910811f82465d031f
math-pixel <59537610+math-pixel@users.noreply.github.com>
2024-02-17 00:14:10 +0100

    Convert whisper with context

--- transcriber/whisperTranscriber.py
+++ transcriber/whisperTranscriber.py
@@ -1,32 +1,35 @@
-from interfaceTranscriber import InterfaceTranscriber
+from transcriber.interfaceTranscriber import InterfaceTranscriber
 import whisper
 from DLog import *
 
 class Whisper(InterfaceTranscriber):
 
-    def __init__(self,fileName,callback, model = "small"):
+    def __init__(self, customContext):
         self.avaibleModelForTranscriber = [ "tiny", "base",  "small", "medium", "large" ]
-        self.callback = callback
-        self.fileName = fileName
-        self.model = None
-
-        self.loadModel(model)
+        self.callback = customContext.myCallback
+        self.model = customContext.myModel
 
     # load model
-    def loadModel(self, model):
+    def loadModel(self):
 
         # test if model is valid
-        if model in self.avaibleModelForTranscriber: 
-            self.model = whisper.load_model(model)
-            DLog.goodlog(f"model '{model}' loaded succesfully")
+        if self.model in self.avaibleModelForTranscriber: 
+            DLog.goodlog(f"model '{ self.model }' loading")
+            self.model = whisper.load_model(self.model)
+            DLog.goodlog(f"model loaded succesfully")
         else:
-            DLog.error(f"Error : model '{model}' not in avaible list !!! \n if you are develloper add it in 'avaibleModelForTranscriber' array of your custom AI")
+            DLog.error(f"Error : model '{self.model}' not in avaible list !!! \n if you are develloper add it in 'avaibleModelForTranscriber' array of your custom AI")
 
+    def changeModel(self, model):
+
+        if self.model in self.avaibleModelForTranscriber: 
+            self.model = model
+            self.loadModel() 
 
     # transcribe
-    def transcribe(self):
+    def transcribe(self, filename):
         try:
-            result = self.model.transcribe(self.fileName)
+            result = self.model.transcribe(filename)
             self.callback(result["text"])
         except:
             DLog.error(f"Error : When module trannscribe")
