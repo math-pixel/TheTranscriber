61fe30b581a80656cf965c50bff0c7e965b1d469
JackyTuneur <deku.private@gmail.com>
2024-02-22 10:08:41 +0100

    feat(analyzer): analyzer can analyze video file

--- convertor/ConversionAnalyzer.py
+++ convertor/ConversionAnalyzer.py
@@ -1,5 +1,6 @@
 from DLog import *
 from convertor.ConvertStrategy import *
+import magic
 
 
 class ConversionAnalyzer:
@@ -7,19 +8,35 @@ class ConversionAnalyzer:
         pass
 
     def analyze(self, trContext, convertor):
-        DLog.goodlog("Start analyze the file: " + ("" if trContext.audioPath == None else trContext.audioPath))
-        # - Ce que je fais temporairement
-        convertor.setConvertStrategy(MP4ToMP3ConvertStrategy())
+        DLog.goodlog("Start analyze the file")
+        # If we don't have audio path we must check how to convert
+        if trContext.audioPath != None :
+            return False;
 
-        return True
-        # - Ce qu'il peut être fait ensuite
-        # ------
-        # Ici on peut imaginer du code du genre
-        # if path == mp4:
-        #   self.convertor.setStrategy(MP4toMP3Strategy())
-        #   Return True
-        # ...
-        # Et ainsi de suite pour pouvoir set la bonne "strategie"
-        # Et si à la fin aucune stratégie n'est set, on return False
+        # Verify the mime type of the file
+        file_type = self.get_file_type(trContext.videoPath);
+        DLog.goodlog(f"File type of the video : {file_type}")
+        if file_type == None :
+            DLog.error("File doesn't have a mime type ? Check if the file you wan't to analyze exist")
+
+        if file_type == "video/mp4":
+            convertor.setConvertStrategy(MP4ToMP3ConvertStrategy())
+            return True;
+        elif file_type == "video/avi":
+            convertor.setConvertStrategy(AviToMP3ConvertStrategy())
+            return True;
+        else :
+            DLog.error("videoPath of trContext isn't convertible.")
+
         return False
-        pass
+
+    
+    def get_file_type(self, videoPath):
+        # Try catch lol
+        try:
+            mime = magic.Magic(mime=True)
+            file_type = mime.from_file(videoPath)
+            return file_type
+        except Exception as e:
+            DLog.error(f"An error occurred in the analyze of the file : {e}")
+            return None
