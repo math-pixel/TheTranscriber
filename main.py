e0cdb4ab497001c1c9adaf71fdd3b74f5a09d045
Pierre-Louis ROUSSEAUX <pl74rou@gmail.com>
2024-03-10 18:56:38 +0100

    feat(global): includes all features

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 from TranscriberContext import *
+from downloader.DownloaderManager import *
+from convertor.ConversionCoordinator import *
+from transcriber.transcriberManager import *
 
 transcriberContext = TranscriberContext.getContextWithUrl()
+
+downloaderManager = DownloaderManager()
+downloaderManager.startDownload(transcriberContext)
+
+converterCoordinator = ConversionCoordinator.getConversionCoordinator()
+conversionResult = converterCoordinator.convert(transcriberContext)
+
+
+def myEndedFunctionDamour(result):
+    print(f"result of transcriber : {result['text']}")
+    tsContext.transcribeText = result["segments"]
+
+
+contextTranscriber = IATranscriberContext("small", myEndedFunctionDamour)
+myManager = TranscriberManager()
+myManager.useAI(ListAI.WHISPER, contextTranscriber)
+myManager.transcribe(transcriberContext.audioPath)
